11755be9126d4e7baf14cc67859e4d199eb62528
tavolafourcade <tavolafourcade@gmail.com>
2021-07-21 00:56:38 +0000

    Write a function that measures the size of a binary tree
    Prototype: size_t binary_tree_size(const binary_tree_t *tree);
    Where tree is a pointer to the root node of the tree to measure the size
    If tree is NULL, the function must return 0

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - Measure the size of a binary tree.
+ * @tree: A pointer to the parent node.
+ *
+ * Return: Tree's size.
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0;
+
+	if (tree)
+	{
+		size += 1;
+		size += binary_tree_size(tree->left);
+		size += binary_tree_size(tree->right);
+	}
+	return (size);
+}
